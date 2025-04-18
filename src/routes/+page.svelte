16458d0b5f14685f5e78e02fd18d5150eda3b369
jonathanschimpf <jonathan@jonathanschimpf.com>
2025-04-18 10:42:29 -0400

    whatever

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,4 @@
 <!-- src/routes/+page.svelte -->
-<script>
-	// Nothing needed here unless you want logic later
-</script>
-
-<!-- ✅ Homepage image when loading root -->
 <section class="homepage">
 	<img src="/photographs/kitchen-tall.png" alt="Kitchen Setup" />
 </section>
