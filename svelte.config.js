d110bc4787920b678302abe7991afe079ad6c602
jonathanschimpf <jonathan@jonathanschimpf.com>
2025-04-18 21:40:02 -0400

    (AGAIN) x1000000000000000000000000000000

--- svelte.config.js
+++ svelte.config.js
@@ -4,10 +4,12 @@ import preprocess from 'svelte-preprocess';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess(),
+
 	kit: {
 		adapter: adapter({
-			split: false // 🛠️ AVOIDS TRYING TO WRITE '_headers/_redirects' INTO BUILD
+			split: false // 🛠️ PREVENTS CONFLICTS WRITING _headers/_redirects
 		}),
+
 		alias: {
 			$lib: './src/lib'
 		}
